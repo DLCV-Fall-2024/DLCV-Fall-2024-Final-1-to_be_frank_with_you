b03380598ff019a71bcab821be0da9a6393a6f7c
tragic_coder <96676144+NTUBOY@users.noreply.github.com>
2024-12-12 01:10:12 +0800

    Update depth.py

--- src/models/encoder/depth.py
+++ src/models/encoder/depth.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from transformers import AutoModel, AutoImageProcessor
+from transformers import AutoImageProcessor, AutoModelForDepthEstimation
 
 from .base import VisionEncoder
 
@@ -9,7 +9,7 @@ class DepthEncoder(VisionEncoder):
     def __init__(
         self,
         model_id: Optional[str] = None,
-        model: Optional[AutoModel] = None,
-        processor: Optional[AutoImageProcessor] = None,
+        model: Optional[AutoModelForDepthEstimation] = AutoModelForDepthEstimation.from_pretrained("depth-anything/Depth-Anything-V2-Small-hf"),
+        processor: Optional[AutoImageProcessor] = AutoImageProcessor.from_pretrained("depth-anything/Depth-Anything-V2-Small-hf"),
     ):
         super().__init__(model_id, model, processor)
